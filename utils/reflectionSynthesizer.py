5afe8622afc26c5e6963cf14ee1e5f66b15aa3e7
chenhao <cs_chenhao@mail.scut.edu.cn>
2020-12-28 15:43:30 +0800

    实现评估结果保存xls

--- utils/reflectionSynthesizer.py
+++ utils/reflectionSynthesizer.py
@@ -218,8 +218,8 @@ class ReflectionSynthesizer:
 
 # test
 if __name__ == '__main__':
-    s = ReflectionSynthesizer(out_path='../SynDataset/out/1')
-    for i in range(200):
+    s = ReflectionSynthesizer(out_path='../SynDataset/out/eval-syn')
+    for i in range(256):
         print(i)
         next(s)
 
